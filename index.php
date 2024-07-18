3af4f080f72c177df94f201d09faaea003786a3a
root <root@host.ultaserver.net>
2024-07-18 07:26:41 -0400

    first

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    image
+<img src="https://instagirl.net/storage/files/shares/224/xiuren-no5479-yang-chen-chen-yome-79-anh/1XIUREN-No.5479-Yang-Chen-Chen-Yome-MrCong.com-003.webp">
+
+    <img src="https://instagirl.net/storage/files/shares/7528/1Coser-sama31598328-MrCong.com-021.jpeg" alt="">
+<?php
+$a = file_get_contents("https://instagirl.net/storage/files/shares/7528/1Coser-sama31598328-MrCong.com-021.jpeg"); 
+echo $a;
+?>
+</body>
+</html>
